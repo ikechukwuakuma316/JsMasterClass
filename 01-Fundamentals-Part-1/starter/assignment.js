0fc7641905b55bac0156f6569a0c2b79b580df83
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2023-01-02 09:45:06 +0100

    data types

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,6 +1,16 @@
-let country = "Nigeria";
-let continent = "Africa";
-let population = 10000000;
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = "Nigeria";
+// let continent = "Africa";
+// let population = 10000000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+console.log(typeof null);
