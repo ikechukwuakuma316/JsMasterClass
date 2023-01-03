2f2dad589305b2b3c0f379f2ece1f3805185204c
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2023-01-03 12:15:59 +0100

    Template Literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -112,7 +112,7 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2
 console.log(ageJonas, ageSarah, averageAge);
-**/
+
 
 //////////////////////////////////
 
@@ -131,3 +131,25 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
  
 console.log(BMIMark, BMIJohn, markHigherBMI);
+**/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) 
++ ' year old ' + job + '!';
+console.log(jonas);
+// Template literals
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+ 
+console.log(`Just a regular string...`);
+
+console.log(`String with
+multiple 
+lines`);
+
+//Taking Decisions
